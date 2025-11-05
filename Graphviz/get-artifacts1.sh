f662d26fa94e30ec2788d1a7343bf13c01916453
user <zenex27140@gmail.com>
2025-11-05 11:15:28 +0100

    ajout du deuxième curl

--- Graphviz/get-artifacts1.sh
+++ Graphviz/get-artifacts1.sh
@@ -6,3 +6,11 @@ curl -L \
  https://api.github.com/repos/Mathieu27700/TP_R504/actions/artifacts \
  > gh-artifacts.json
 
+curl -L \
+ -H "Accept: application/vnd.github+json" \
+ -H "Authorization: Bearer $(cat ~/token.gh)" \
+ -H "X-GitHub-Api-Version: 2022-11-28" \
+ "https://api.github.com/repos/Mathieu27700/TP_R504/actions/artifacts/4472114083/zip" \
+ --output artifact.zip
+
+unzip artifact.zip
