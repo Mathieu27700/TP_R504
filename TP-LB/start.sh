df06cfed7a1441060055c145e9a06fae9fb37c16
Mathieu27700 <117992949+Mathieu27700@users.noreply.github.com>
2025-12-09 13:26:53 +0100

    Add start script for Docker network and containers

---
+++ TP-LB/start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "[a] Création du réseau Docker interne"
+docker network create tplb 2>/dev/null
+
+echo "[b] Construction de l'image im-nginx-lb"
+docker build -t im-nginx-lb ../tp-A
+
+echo "[c] Création des dossiers shared1 et shared2"
+mkdir -p shared1 shared2
+
+echo "[d] Création des fichiers index.html"
+echo "<h1>Hello 1</h1>" > shared1/index.html
+echo "<h1>Hello 2</h1>" > shared2/index.html
+
+echo "[e] Lancement des conteneurs nginx1 et nginx2"
+docker run -d --rm \
+    --name nginx1 \
+    -p 81:80 \
+    --network tplb \
+    -v "$(pwd)/shared1:/usr/share/nginx/html" \
+    nginx
+
+docker run -d --rm \
+    --name nginx2 \
+    -p 82:80 \
+    --network tplb \
+    -v "$(pwd)/shared2:/usr/share/nginx/html" \
+    nginx
+
+echo "[f] Lancement du conteneur load-balancer..."
+docker run -d --rm \
+    --name nginx-lb \
+    -p 83:80 \
+    --network tplb \
+    im-nginx-lb
+
+echo "=== Tout est lancé avec succès ==="
