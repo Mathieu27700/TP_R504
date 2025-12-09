80a8188f4a54e61a3c4ca4bbb063e4bef8e84311
Mathieu27700 <zenex27140@gmail.com>
2025-12-09 15:38:52 +0100

    finition boucle de comptage

--- TP-LB/boucle.comptage.sh
+++ TP-LB/boucle.comptage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 count1=0
-count2=0
+
 n=500
 
 for ((i=1;i<=n;i++)); do
@@ -9,17 +9,7 @@ for ((i=1;i<=n;i++)); do
 
     if [[ "$response" == "<h1>Hello 1</h1>" ]]; then
         ((count1++))
-    elif [[ "$response" == "<h1>Hello 2</h1>" ]]; then
-        ((count2++))
-    else
-        echo "Réponse inattendue : $response"
     fi
 done
 
 echo "Réponses Hello 1 : $count1"
-echo "Réponses Hello 2 : $count2"
-
-percent1=$((count1 * 100 / n))
-percent2=$((count2 * 100 / n))
-
-echo "Répartition : Hello 1 = $percent1%, Hello 2 = $percent2%"
